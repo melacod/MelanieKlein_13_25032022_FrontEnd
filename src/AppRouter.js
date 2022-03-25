b52c91f93ffac18ca30a24e765c7cdd41f942cdf
melacod <melaniebordeaux2015@gmail.com>
2022-03-25 14:34:49 +0100

    reprise des pages telle quelle

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import User from './pages/User'
 
 const AppRouter = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/user" element={<User />} />
             </Routes>
         </Router>
     )
---
+++ src/pages/SignIn.js
@@ -0,0 +1,49 @@
+const SignIn = () => {
+    return (
+        <>
+            <nav class="main-nav">
+                <a class="main-nav-logo" href="./index.html">
+                    <img
+                        class="main-nav-logo-image"
+                        src="./img/argentBankLogo.png"
+                        alt="Argent Bank Logo"
+                    />
+                    <h1 class="sr-only">Argent Bank</h1>
+                </a>
+                <div>
+                    <a class="main-nav-item" href="./sign-in.html">
+                        <i class="fa fa-user-circle"></i>
+                        Sign In
+                    </a>
+                </div>
+            </nav>
+            <main class="main bg-dark">
+                <section class="sign-in-content">
+                    <i class="fa fa-user-circle sign-in-icon"></i>
+                    <h1>Sign In</h1>
+                    <form>
+                        <div class="input-wrapper">
+                            <label for="username">Username</label>
+                            <input type="text" id="username" />
+                        </div>
+                        <div class="input-wrapper">
+                            <label for="password">Password</label>
+                            <input type="password" id="password" />
+                        </div>
+                        <div class="input-remember">
+                            <input type="checkbox" id="remember-me" />
+                            <label for="remember-me">Remember me</label>
+                        </div>
+
+                        <button class="sign-in-button">Sign In</button>
+                    </form>
+                </section>
+            </main>
+            <footer class="footer">
+                <p class="footer-text">Copyright 2020 Argent Bank</p>
+            </footer>
+        </>
+    )
+}
+
+export default SignIn
