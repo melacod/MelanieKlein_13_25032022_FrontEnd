a9130cbd8a6f50daa6606bab9d2f32a8c9eb886e
melacod <melaniebordeaux2015@gmail.com>
2022-03-26 15:19:19 +0100

    Mise en Place Page SIgnIn

---
+++ src/layout/MainBlack.js
@@ -0,0 +1,11 @@
+import SignInModal from '../components/SignInModal'
+
+const MainBlack = () => {
+    return (
+        <main class="main bg-dark">
+            <SignInModal />
+        </main>
+    )
+}
+
+export default MainBlack
